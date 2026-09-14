600adf087ee1459bcf58acca23f1f583655f001a
syatimwabinlikambo <syatimwabinlikambo@gmail.com>
2026-09-14 13:39:50 +0200

    Backend With Authorised Frontend Origins

--- src/main/java/com/syatimwaraph/stock_sale_mgnt_v1/security/CorsConfig.java
+++ src/main/java/com/syatimwaraph/stock_sale_mgnt_v1/security/CorsConfig.java
@@ -22,7 +22,8 @@ public class CorsConfig {
                         "http://localhost:5500",
                         "http://127.0.0.1:5500",
                         "http://127.0.0.1:57500",
-                        "http://localhost:57500"
+                        "http://localhost:57500",
+                        "https://stock-sale-mgnt-frontend.onrender.com:80"
                 )
         );
 
